6e9bcc2814dbdde91ca27355ac072abdb919ab7c
Ema Eiliakas <eeiliakas@gmail.com>
2025-04-29 21:09:20 +0100

    🚧 Making the constraints for camera usage more specific

--- js/camera.js
+++ js/camera.js
@@ -2,13 +2,15 @@ export const videoElement = document.getElementById('input_video');
 export const canvasElement = document.getElementById('output_canvas');
 export const canvasCtx = canvasElement.getContext('2d');
 
-// Camera constraints
+const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+
 const constraints = {
-  video: {
-    facingMode: { ideal: 'environment' }
-  },
+  video: isMobile
+    ? { facingMode: { ideal: 'environment' } }
+    : true, // Default camera on desktop
   audio: false
 };
+
 
 // Start the camera stream
 export function startCamera() {
