f819eb8340f7a634a88aebb0aba5cc0cb0cf88d5
Ema Eiliakas <eeiliakas@gmail.com>
2025-05-08 23:18:58 +0100

    gsgsfsfsfsfsfsfsfs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-import { videoElement } from './camera.js';
+import { videoElement, startCamera } from './camera.js';
 import { onResults } from './handDetection.js';
 
-// Initialize Mediapipe Hands
 const hands = new Hands({
   locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
 });
@@ -15,13 +14,18 @@ hands.setOptions({
 
 hands.onResults(onResults);
 
-// Remove manual getUserMedia; let Camera handle it
-const camera = new Camera(videoElement, {
-  onFrame: async () => {
-    await hands.send({ image: videoElement });
-  },
-  width: 1280,
-  height: 720
-});
+async function init() {
+  await startCamera(); // startCamera sets up the correct stream
 
-camera.start();
+  // Begin processing frames
+  function processFrame() {
+    if (videoElement.readyState >= 2) {
+      hands.send({ image: videoElement });
+    }
+    requestAnimationFrame(processFrame);
+  }
+
+  requestAnimationFrame(processFrame);
+}
+
+init();
